c00e883b54161fe2e757cd8b154b8908cd93233c
vadimyt <pvcs@yandex.ru>
2023-05-29 03:15:57 +0300

    WindowsAppMinSize

--- Studenda/App.xaml.cs
+++ Studenda/App.xaml.cs
@@ -8,4 +8,38 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+        window.Activated += Window_Activated;
+        return window;
+    }
+
+    private async void Window_Activated(object sender, EventArgs e)
+    {
+#if WINDOWS
+        const int DefaultWidth = 1600;
+        const int DefaultHeight = 800;
+
+        const int MinimumWidth = 1050;
+        const int MinimumHeight = 700;
+
+        var window = sender as Window;
+
+        // change window size.
+        window.Width = DefaultWidth;
+        window.Height = DefaultHeight;
+
+        window.MinimumWidth = MinimumWidth;
+        window.MinimumHeight = MinimumHeight;
+        // give it some time to complete window resizing task.
+        await window.Dispatcher.DispatchAsync(() => { });
+
+        var disp = DeviceDisplay.Current.MainDisplayInfo;
+
+        // move to screen center
+        window.X = (disp.Width / disp.Density - window.Width) / 2;
+        window.Y = (disp.Height / disp.Density - window.Height) / 2;
+#endif
+    }
 }
