6ea0185415217615c19fdfb3753dd4a63966616e
N0tilT <timtompson3241@gmail.com>
2023-06-03 15:32:22 +0300

    renaming

---
+++ Studenda/ViewModel/UIConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studenda.ViewModel
+{
+    public class UIConverter
+    {
+
+    }
+
+    //<Label Text = "Вторник"
+    //               Style="{StaticResource DayLabelTheme}"/>
+    //        <Grid HorizontalOptions = "Start"
+    //              BackgroundColor="White"
+    //              HeightRequest="300"
+    //              WidthRequest="350">
+
+    //        </Grid>
+}
