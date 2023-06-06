1d8f46209108e64b871d8055fa2de6defb8fb3a9
vadimyt <pvcs@yandex.ru>
2023-06-06 15:41:41 +0300

    LogOut

--- Studenda/AppShell.xaml.cs
+++ Studenda/AppShell.xaml.cs
@@ -1,14 +1,19 @@
 ﻿using Studenda.View;
+using Studenda.ViewModel;
 
 namespace Studenda;
 
 public partial class AppShell : Shell
 {
+	AppShellViewModel vm = new AppShellViewModel();
 	public AppShell()
 	{
 		InitializeComponent();
 
-		Routing.RegisterRoute(nameof(ScheduleView), typeof(ScheduleView));
+        BindingContext = vm;
+
+        Routing.RegisterRoute(nameof(ScheduleView), typeof(ScheduleView));
         Routing.RegisterRoute(nameof(ProfileView), typeof(ProfileView));
+        Routing.RegisterRoute(nameof(LogInView), typeof(LogInView));
     }
 }
