703381825f1558a0054eca187f890ecd26c9ff7f
N0tilT <timtompson3241@gmail.com>
2023-06-11 16:03:13 +0300

    ProfileView

--- Studenda/View/AndroidView/ProfileView.xaml.cs
+++ Studenda/View/AndroidView/ProfileView.xaml.cs
@@ -1,9 +1,14 @@
+using Studenda.ViewModel;
+
 namespace Studenda.View.AndroidView;
 
 public partial class ProfileView : ContentPage
 {
+	ProfileViewModel vm = new ProfileViewModel();
 	public ProfileView()
 	{
 		InitializeComponent();
+
+		BindingContext = vm;
 	}
 }
